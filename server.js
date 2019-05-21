824b02631888e84630b170cf3148d7659406e031
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-05-21 15:30:23 +0530

    Updated - Separate modules created

--- server.js
+++ server.js
@@ -1,49 +1,25 @@
 var express = require('express');
 var app = express();
-const https = require('https');
-const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
-app.get('/getOTP/:phonenumber', function (req, res) {
-   console.log('came here');
-   if(req.params.phonenumber) {
+//Initialize ALL routes including subfolders
+var fs = require('fs');
+var path = require('path');
 
+function recursiveRoutes(folderName) {
+    fs.readdirSync(folderName).forEach(function(file) {
 
-        // Result from isPossibleNumber().
-        const number = phoneUtil.parseAndKeepRawInput(req.params.phonenumber);
-        if(phoneUtil.isPossibleNumber(number) && phoneUtil.isValidNumber(number) &&
-            phoneUtil.isValidNumberForRegion(number, 'IN')) {
-                var otpRequestUrl = 'https://2factor.in/API/V1/66b88c13-ed3c-11e8-a895-0200cd936042/SMS/' + req.params.phonenumber + '/AUTOGEN/PhoneAuthLoudSpeaker';
-                console.log(otpRequestUrl);
-                https.get(otpRequestUrl, (resp) => {
-                    let data = '';
+        var fullName = path.join(folderName, file);
+        var stat = fs.lstatSync(fullName);
 
-                    // A chunk of data has been recieved.
-                    resp.on('data', (chunk) => {
-                        data += chunk;
-                        console.log(data);
-                    });
-
-                    // The whole response has been received. Print out the result.
-                    resp.on('end', () => {
-                        var response = JSON.parse(data);
-                        res.end(response.Details);
-                    });
-                }).on("error", (err) => {
-                    console.log("Error: " + err.message);
-                });
-
-        } else {
-            res.end('{"error":"invalid phone number"}');
+        if (stat.isDirectory()) {
+            recursiveRoutes(fullName);
+        } else if (file.toLowerCase().indexOf('.js')) {
+            require('./' + fullName)(app);
+            console.log("require('" + fullName + "')");
         }
-    } else {
-        res.end('{"error":"invalid phone number"}');
-    }
-});
-
-app.get('/Users/:id', function (req, res) {
-    console.log(req.params.id);
-    res.end('<h1>Some Title<h1>');
- });
+    });
+}
+recursiveRoutes('routes'); // Initialize it
 
 var server = app.listen(8081, function () {
    var host = server.address().address

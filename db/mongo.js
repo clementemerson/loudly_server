d5e0c93345711b004eab8b2d401270bf45f11b30
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-05-21 16:53:10 +0530

    initial checkin mongo.js

---
+++ db/mongo.js
@@ -0,0 +1,23 @@
+//mongo
+var MongoClient = require('mongodb').MongoClient;
+var mongodb;
+
+module.exports = {
+    mongodb: mongodb,
+    initDbConnection: function(callback) {
+        // var connectionUrl;
+        // // Create the database connection
+        // MongoClient.connect(connectionUrl, {
+        //     poolSize: 10
+        //     // other options can go here
+        // }, function (err, db) {
+        //     assert.equal(null, err);
+        //     mongodb = db;
+        //     callback();
+        // }
+        // );
+
+        mongodb = 'Clement';
+        callback();
+    },    
+};

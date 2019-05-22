2a69eaf0cf6b9ad568ed1474ee88217baa016d94
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-05-22 17:42:19 +0530

    Add files via upload

---
+++ routes/errorstousers.js
@@ -0,0 +1,14 @@
+module.exports = {
+    invalidData: {
+        Status: 'Error',
+        Details: 'Invalid Data'
+    },
+    invalidPhoneNumber: {
+        Status: 'Error',
+        Details: 'Invalid phone number'
+    },
+    errorInProcessing: {
+        Status: 'Error',
+        Details: 'Error in processing request'
+    },
+};
---
+++ routes/successtousers.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sendData: function (data) {
+        return {
+            Status: 'Success',
+            Data: data
+        };
+    },
+};

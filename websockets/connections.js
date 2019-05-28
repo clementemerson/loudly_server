53d2961f9f63301d633d278f2232660eea65cc94
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-05-28 17:18:41 +0530

    Add files via upload

--- websockets/connections.js
+++ websockets/connections.js
@@ -3,5 +3,12 @@ var connections = {}
 module.exports = {
     getConnections: () => {
         return connections;
+    },
+
+    inform: async (listOfUsers, data) => {
+        listOfUsers.forEach(eachUser => {
+            if(connections[eachUser.user_id]) 
+                connections[eachUser.user_id].send(JSON.stringify(data));
+        });
     }
 }

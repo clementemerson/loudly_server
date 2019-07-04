8d39f97cb59b151219bd135bb0bbf761f7efacf5
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-07-04 12:43:29 +0530

    Add files via upload

--- websockets/connections.js
+++ websockets/connections.js
@@ -1,5 +1,6 @@
-var connections = new Map();
-var pollResultSubscriptions = {};
+let connections = new Map();
+let pollResultSubscriptions = {};
+let userPollSubscriptions = {};
 
 module.exports = {
     getConnections: () => {
@@ -10,10 +11,36 @@ module.exports = {
         return pollResultSubscriptions;
     },
 
+    getUserPollSubscriptions: () => {
+        return userPollSubscriptions;
+    },
+
     inform: async (listOfUsers, data) => {
         listOfUsers.forEach(eachUser => {
             if (connections.get(eachUser.user_id))
                 connections.get(eachUser.user_id).send(JSON.stringify(data));
         });
+    },
+
+    unsubscribeUserSubscriptions: (user_id) => {
+        console.log('connections.unsubscribeUserSubscriptions');
+        const pollArray = userPollSubscriptions[user_id];
+        if (pollArray) {
+            pollArray.forEach(pollid => {
+                try {
+                    //Remove entry from pollresult subscription
+                    const subscribersArray = pollResultSubscriptions[pollid];
+                    if (subscribersArray) {
+                        var index = subscribersArray.indexOf(user_id);
+                        if (index > -1)
+                            subscribersArray.splice(index, 1);
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+        }
+
+        delete userPollSubscriptions[user_id];
     }
 }

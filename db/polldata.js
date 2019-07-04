395c3c7d295896d23cd50f9fcbcbbcd64f2f841b
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-07-04 17:41:31 +0530

    Add files via upload

--- db/polldata.js
+++ db/polldata.js
@@ -11,6 +11,7 @@ module.exports = {
         await mongodb().collection(dbtables.PollData).insertOne({
             pollid: data.pollid,
             title: data.title,
+            isdeleted: false,
             resultispublic: data.resultispublic,
             canbeshared: data.canbeshared,
             createdby: data.createdby,
@@ -34,5 +35,50 @@ module.exports = {
         return await mongodb().collection(dbtables.PollData)
             .find({ pollid: { $in: pollids } })
             .toArray();
-    }
+    },
+
+    isCreator: async (data) => {
+        console.log('db.polldata.isCreator');
+        let isCreator = await mongodb().collection(dbtables.PollData)
+            .find({
+                createdby: data.user_id,
+                pollid: data.pollid
+            })
+            .toArray();
+
+        if (isCreator[0])
+            return true;
+        else
+            return false;
+    },
+
+    delete: async (data) => {
+        console.log('db.polldata.delete');
+        let date = new Date();
+        let updatedAt = date.getTime();
+
+        await mongodb().collection(dbtables.PollData).updateOne(
+            { pollid: data.pollid },
+            {
+                $set: {
+                    isdeleted: true,
+                    updatedAt: updatedAt
+                }
+            });
+    },
+
+    isDeleted: async (data) => {
+        console.log('db.polldata.isDeleted');
+        let isDeleted = await mongodb().collection(dbtables.PollData)
+            .find({
+                pollid: data.pollid,
+                isdeleted: true
+            })
+            .toArray();
+
+        if (isDeleted[0])
+            return true;
+        else
+            return false;
+    },
 }

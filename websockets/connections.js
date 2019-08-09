b2fd6320198945e2c1106c10b619feb66db86865
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-08-09 16:38:53 +0530

    Add files via upload

--- websockets/connections.js
+++ websockets/connections.js
@@ -1,46 +1,13 @@
 let connections = new Map();
-let pollResultSubscriptions = {};
-let userPollSubscriptions = {};
 
 module.exports = {
     getConnections: () => {
         return connections;
     },
 
-    getPollResultSubscriptions: () => {
-        return pollResultSubscriptions;
+    inform: async (user_id, data) => {
+        const wsConn = connections.get(user_id);
+        if (!!wsConn)
+            connections.get(user_id).send(JSON.stringify(data));
     },
-
-    getUserPollSubscriptions: () => {
-        return userPollSubscriptions;
-    },
-
-    inform: async (listOfUsers, data) => {
-        listOfUsers.forEach(eachUser => {
-            if (connections.get(eachUser.user_id))
-                connections.get(eachUser.user_id).send(JSON.stringify(data));
-        });
-    },
-
-    unsubscribeUserSubscriptions: (user_id) => {
-        console.log('connections.unsubscribeUserSubscriptions');
-        const pollArray = userPollSubscriptions[user_id];
-        if (pollArray) {
-            pollArray.forEach(pollid => {
-                try {
-                    //Remove entry from pollresult subscription
-                    const subscribersArray = pollResultSubscriptions[pollid];
-                    if (subscribersArray) {
-                        var index = subscribersArray.indexOf(user_id);
-                        if (index > -1)
-                            subscribersArray.splice(index, 1);
-                    }
-                } catch (err) {
-                    console.log(err);
-                }
-            });
-        }
-
-        delete userPollSubscriptions[user_id];
-    }
 }

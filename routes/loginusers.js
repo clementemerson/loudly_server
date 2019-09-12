8d64e8729042b0e4315e8bcfc4479147ee139402
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-09-12 16:09:16 +0530

    Add files via upload

--- routes/loginusers.js
+++ routes/loginusers.js
@@ -1,12 +1,9 @@
-
 const LoginController = require('../controllers/LoginController');
 const jwtController = require('../controllers/jwtController');
 
-const asyncMiddleware = (fn) =>
-  (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-        .catch(next);
-  };
+const asyncMiddleware = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 
 module.exports = function(app) {
   // Comment:
@@ -14,9 +11,7 @@ module.exports = function(app) {
   //        verifies it (only an indian number is valid)
   // 2. Stores the required info in db
   // 3. Sends a reply back to client
-  app.get('/getotp/:phonenumber', [
-    LoginController.getotp,
-  ]);
+  app.get('/getotp/:phonenumber', [LoginController.getotp]);
 
   app.post('/signin', [
     LoginController.verifyotp,

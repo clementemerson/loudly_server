4dbbfa829750fde6e3272adc0d9cbfeff040b19a
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-09-13 11:41:54 +0530

    Create group_addUser.test.js

---
+++ apitests/group_addUser.test.js
@@ -0,0 +1,81 @@
+const expect = require('expect');
+const testUtil = require('./testutil');
+
+const errors = require('../helpers/errorstousers');
+const success = require('../helpers/successtousers');
+
+beforeAll(async () => {
+    console.log = () => { };
+});
+
+beforeEach(() => { });
+
+afterEach(() => { });
+
+afterAll(() => { });
+
+describe('addUser', () => {
+    test('should fail, if useridsToAdd is undefined', async () => {
+
+    });
+
+    test('should fail, if useridsToAdd is null', async () => {
+
+    });
+
+    test('should fail, if useridsToAdd array has string in it', async () => {
+
+    });
+
+    test('should fail, if groupid is undefined', async () => {
+
+    });
+
+    test('should fail, if groupid is null', async () => {
+
+    });
+
+    test('should fail, if groupid is string', async () => {
+
+    });
+
+    test('should fail, if permission is undefined', async () => {
+
+    });
+
+    test('should fail, if permission is null', async () => {
+
+    });
+
+    test('should fail, if permission is number', async () => {
+
+    });
+
+    test('should fail, if permission is string but invalid', async () => {
+
+    });
+
+    test('should fail, if user is not an ADMIN of the group', async () => {
+
+    });
+
+    test('should fail, and abort transaction on error', async () => {
+
+    });
+
+    test('should add all users to the group in normal condition', async () => {
+
+    });
+
+    test('should filter users whose accounts are not exist', async () => {
+
+    });
+
+    test('should filter users who are already member of the group', async () => {
+
+    });
+
+    test('should fail, if no users need to be added to the group', async () => {
+
+    });
+});

1ba7553ff644ac8d95629e879af3924ac5babe17
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-09-24 17:16:58 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mongoSettings,
+    redisSettings
+}
+
+const mongoSettings = {
+    url: 'mongodb://loudly.loudspeakerdev.net:27017',
+    user: 'mongoclient',
+    pwd: 'knvf532R85HF02SFD4G3YkjJlsFdvnKJeL9847F4F39fnjLi;kzcDvFsd96gPdsPfhRchgsdrfJte5tyGnTdfkgbd4f5nghdGfGjio62tj',
+    db: 'loudly'
+};
+const redisSettings = {
+    url: 'loudly.loudspeakerdev.net',
+    port: 6379,
+    db: 0,
+    pwd: 'jbhfjahfje8243752bdsaBHFJ754KJGNJGDF8673BJGgijuhgjihuuit'
+}

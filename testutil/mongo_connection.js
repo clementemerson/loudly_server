e566b4cae10b4906a99e98f5b826daa9480ef7c8
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-09-24 17:12:38 +0530

    Add files via upload

--- testutil/mongo_connection.js
+++ testutil/mongo_connection.js
@@ -1,14 +1,13 @@
 
 const mongo = require('../db/mongo');
 
-const testUser = 'testUser';
-const pwdTestUser = 'dnsdjfjh243852nmwefqSNha798867bjah87NU7YIUJBH&FHHLKJDIOFKLJkdsjg435j4kjvhsd9uewnkdjfisdjrjgjGTYjubn35jnhsdksaf';
-const testDb = 'loudlyTest';
-const connectionUrl = 'mongodb://loudly.loudspeakerdev.net:27017';
-
 module.exports = {
     getDbConnection: () => mongo.getDbConnection(),
     initDbConnection: async () => await
-        mongo.initDbConnection(connectionUrl, testUser, pwdTestUser, testDb),
+        mongo.initDbConnection(
+            'mongodb://loudly.loudspeakerdev.net:27017',
+            'testUser',
+            'dnsdjfjh243852nmwefqSNha798867bjah87NU7YIUJBH&FHHLKJDIOFKLJkdsjg435j4kjvhsd9uewnkdjfisdjrjgjGTYjubn35jnhsdksaf',
+            'loudlyTest'),
     getDbClient: () => mongo.getDbClient(),
 }

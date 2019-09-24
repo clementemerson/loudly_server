a9edb091f143da416a21a7e348fa3c7a1d4e724a
clementemerson <44707413+clementemerson@users.noreply.github.com>
2019-09-24 17:16:40 +0530

    Create apitest.js

---
+++ config/apitest.js
@@ -0,0 +1,28 @@
+module.exports = {
+    mongoSettings: {
+        url: 'mongodb://loudly.loudspeakerdev.net:27017',
+        user: 'mongoclient',
+        pwd: 'knvf532R85HF02SFD4G3YkjJlsFdvnKJeL9847F4F39fnjLi;kzcDvFsd96gPdsPfhRchgsdrfJte5tyGnTdfkgbd4f5nghdGfGjio62tj',
+        db: 'loudly'
+    },
+    redisSettings: {
+        url: 'loudly.loudspeakerdev.net',
+        port: 6379,
+        db: 5,
+        pwd: 'jbhfjahfje8243752bdsaBHFJ754KJGNJGDF8673BJGgijuhgjihuuit'
+    },
+}
+
+// mongoSettings: {
+//     url: 'mongodb://loudly.loudspeakerdev.net:27017',
+//     user: 'testUser',
+//     pwd: 'dnsdjfjh243852nmwefqSNha798867bjah87NU7YIUJBH&FHHLKJDIOFKLJkdsjg435j4kjvhsd9uewnkdjfisdjrjgjGTYjubn35jnhsdksaf',
+//     db: 'loudlyTest'
+// },
+
+const mongoSettings = {
+    url: 'mongodb://loudly.loudspeakerdev.net:27017',
+    user: 'mongoclient',
+    pwd: 'knvf532R85HF02SFD4G3YkjJlsFdvnKJeL9847F4F39fnjLi;kzcDvFsd96gPdsPfhRchgsdrfJte5tyGnTdfkgbd4f5nghdGfGjio62tj',
+    db: 'loudly'
+};
